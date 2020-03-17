7d2c23a4341cceeff64824208382bca4549d4f66
Cathy Tasama <ctasama@cnm.edu>
2020-03-17 11:25:37 -0600

    created a file for pseudocode example.

---
+++ pseudocode-ex.php
@@ -0,0 +1,24 @@
+//Get the age of the person.
+/*
+$a = age("Years");
+if ($a <= "12") {
+	echo "FREE";
+} elseif ($a > "12") {
+	echo "$6";
+} elseif ($a > "64") {
+	echo "$5";
+}
+*/
+
+// Write an algorithm that runs from 1 to 20. When the number is even,
+output 'fizz'. When the number is divisible by 5, output 'buzz'.
+If it is neither even nor divisible by five, output 'foo'.
+
+for $number from 1 to 20;
+	while number is even; {
+		echo "fizz";
+	}while $number is divisible by 5; {
+		echo "buzz";
+	}while $number is neither even nor divisible by 5; {
+		echo "foo";
+}
